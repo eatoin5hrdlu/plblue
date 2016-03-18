7aff3fd0be5cd287f9875b22a9dbc8e9af6e17c4
Peter Reintjes <peter.reintjes@ncmls.org>
2016-03-18 18:05:05 -0400

    includes and debug

--- plbluelinux.h
+++ plbluelinux.h
@@ -5,6 +5,8 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
 #include <bluetooth/rfcomm.h>
 
 static struct sockaddr_rc addr = {
